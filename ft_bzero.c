4d3393d5502bbc5b2b45ad00d817dd824a0b615f
Sabino Monte Engombe <smonte-e@c2r1s7.42lausanne.ch>
2022-11-01 17:36:36 +0100

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,45 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smonte-e <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 16:47:26 by smonte-e          #+#    #+#             */
-/*   Updated: 2022/11/01 16:49:12 by smonte-e         ###   ########.fr       */
+/*   Created: 2022/11/01 17:15:00 by smonte-e          #+#    #+#             */
+/*   Updated: 2022/11/01 17:29:57 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
+#include <unistd.h>
 
-void *ft_memset(void *str, int c, size_t n)
+void *ft_bzero(void *str, size_t n)
 {
-    unsigned int i;
-    unsigned char *buffer;
+	if (n == '0')
+		return (str);
+	else
+	{
+		unsigned int i;
+		unsigned char *buffer;
+	
+		i = 0;
+    	buffer = str;
 
-    i = 0;
-    buffer = str;
-
-    while (i < n)
-    {
-        buffer[i] = (unsigned char)c;
-        i++;
-    }
+    	while (i < n)
+    	{
+        	buffer[i] = '\0';
+        	i++;
+    	}
     return (buffer);
+	}
 }
 
-/*
+#include <string.h>
+#include <stdio.h>
 int main()
 {
     int n = 10;
     int arr[n];
   
-    ft_memset(arr, 0, n*sizeof(arr[0]));
-    printf("\nArray after ft_memset()\n");
+    ft_bzero(arr, 7);
+    printf("\nArray after ft_bzero()\n");
 	for (int i=0; i<n; i++)
       printf("%d ", arr[i]);  
 	unsigned char	string[48] = "heguyzzhorudoinlmaoooilikepizza";
-	ft_memset(string, '0', 15*sizeof(string[0]));
-	printf("\n\nString after ft_memset()\n%s\n", string);
+	ft_bzero(string, 15*sizeof(string[0]));
+	printf("\n\nString after ft_bzero()\n%s\n", string);
+	unsigned char	nonString[48] = "heguyzzhorudoinlmaoooilikepizza";
+	ft_bzero(nonString, 0);
+	printf("\n\nString after ft_bzero()\n%s\n", nonString);
     return 0;
-}*/
+}
