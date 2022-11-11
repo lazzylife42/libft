aad3c6275f7cc61c9a55f660605fb639be744a7d
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2022-11-11 20:04:12 +0100

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 18:32:30 by smonte-e          #+#    #+#             */
+/*   Updated: 2022/11/09 18:32:38 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <sting.h>
+//#include <stdio.h>
+
+strlcat(char *dst, const char *src, size_t size);
+
+char	*ft_strlcat(char * restrict dest, const char * restrict src, size_t dstsize)
+{
+	int	i;
+	char * s;
+	char * d;
+
+    if (dest == NULL)
+		return (dstsize);
+	if (dstsize == 0)
+		return (ft_strlen(src));
+
+	i = 0;
+	s = (char *)src;
+	d = dest;
+
+	}
+	while (i < (int)dstsize - 1)
+	{
+		if (!s[i])
+		{
+			d[i] = 0;
+			return (ft_strlen(src));
+		}
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = 0;
+	return (ft_strlen(src));
+}
