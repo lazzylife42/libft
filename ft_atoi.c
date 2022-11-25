98a80d81f2e2fa10203cfdf13356fd956dad3765
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2022-11-25 19:17:29 +0100

    let's go

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 19:55:27 by smonte-e          #+#    #+#             */
+/*   Updated: 2022/11/12 19:55:58 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	signe;
+	int	cache;
+
+	i = 0;
+	signe = 1;
+	cache = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\f'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signe *= -1;
+		i++;
+	}	
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		cache = cache * 10 + (str[i] - 48);
+		i++;
+	}	
+	return (signe * cache);
+}
+/*
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	printf("%d", ft_atoi(argv[1]));
+}*/
---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 19:35:11 by smonte-e          #+#    #+#             */
+/*   Updated: 2022/11/12 19:35:32 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+    char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)s;
+
+    while (i < n)
+	{
+		if ((unsigned char)str[i] == (unsigned char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
