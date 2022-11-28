6d913d7211a08780d14004e8bee4f3f67b7a93cd
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2022-11-28 22:52:48 +0100

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smonte-e <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 22:37:05 by smonte-e          #+#    #+#             */
+/*   Updated: 2022/11/28 22:37:08 by smonte-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strdup(char *src)
+{
+    char *new;
+    int i;
+
+    i = 0;
+    if(!(new = (char *)malloc(sizeof(char) * ft_strlen(src) + 1)))
+        return (NULL);
+    while(*src)
+        new[i++] = *src++;
+    new[i] = '\0';
+    return (new);
+}
