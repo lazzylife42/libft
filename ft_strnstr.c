befb89e072d30fca354616a44eb267b03216b62a
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2022-12-05 19:27:08 +0100

    strnstr ok

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,16 +17,21 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
-	if (!s2 || !s2[0])
+	if (s2[0] == '\0')
 		return ((char *)s1);
-	while (i < n && s1[i] != '\0')
+	i = 0;
+	while (s1[i] != '\0' && i < n)
 	{
 		j = 0;
-		while (i + j < n && s1[i + j] == s2[j] && s2[j] != '\0')
-			j++;
-		if (s2[j] == '\0')
-			return ((char *)s1 + i);
+		if (s1[i] == s2[j])
+		{
+			while (s1[i + j] == s2[j] && i + j < n)
+			{
+				j++;
+				if (s2[j] == '\0')
+					return ((char *)&s1[i]);
+			}
+		}
 		i++;
 	}
 	return (NULL);
