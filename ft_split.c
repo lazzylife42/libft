2aa803cb436f730f481d37e3ba9e9e207e5d93c3
Sabino Monte Engombe <smonte-e@c1r4s2.42lausanne.ch>
2022-12-12 22:49:30 +0100

    almost split

--- ft_split.c
+++ ft_split.c
@@ -3,80 +3,95 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smonte-e <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: smonte-e <smonte-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 18:10:33 by smonte-e          #+#    #+#             */
-/*   Updated: 2022/12/12 18:10:40 by smonte-e         ###   ########.fr       */
+/*   Updated: 2022/12/12 22:46:17 by smonte-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	word_numb(char *str)
+static char	**free_arr(char **tab)
 {
 	int	i;
-	int	numb;
 
 	i = 0;
-	numb = 0;
-	while (str[i])
+	while (tab[i] != '\0')
 	{
-		while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
-			i++;
-		if (str[i])
-		{
-			while (str[i] && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
-				i++;
-			numb++;
-		}
+		free(tab[i]);
+		i++;
 	}
-	return (numb);
+	free(tab[i]);
+	free(tab);
+	return (NULL);
 }
 
-int	word_len(char *str, int i)
+static int	word_numb(char *str, char c)
 {
-	int	len;
+	char	last;
+	int		i;
+	int		w_count;
 
-	len = 0;
-	while (str[i] && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
+	last = c;
+	i = 0;
+	w_count = 0;
+	while (str[i] != '\0')
 	{
+		if (str[i] != c && last == c)
+			w_count++;
+		last = str[i];
 		i++;
-		len++;
 	}
-	return (len);
+	return (w_count);
+}
+
+static char	*word_dup(char *str, int start, int end)
+{
+	int		i;
+	int		w_size;
+	char	*word;
+
+	i = 0;
+	w_size = end - start;
+	word = (char *)malloc(sizeof(char) * (w_size) + 1);
+	if (!word)
+		return (NULL);
+	while (start < end)
+	{
+		word[i] = (char)str[start];
+		i++;
+		start++;
+	}
+	word[i] = '\0';
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**tab;
-	int		i;
-	int		j;
-	int		k;
+	char	**split;
+	size_t	i;
+	size_t	j;
+	int		flag;
 
-	tab = (char **)malloc((word_numb(s) + 1) * sizeof(char *));
-	if (!tab)
+	split = (char **)malloc((word_numb(s, c) + 1) * sizeof(char *));
+	if (!s || !split)
 		return (NULL);
-	i = 0;
+	i = -1;
 	j = 0;
-	while (s[i])
-	{	
-		k = 0;
-		while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-			i++;
-		if (s[i])
+	flag = -1;
+	while (i++ <= ft_strlen(s))
+	{
+		if (s[i] != c && flag < 0)
+			flag = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && flag >= 0)
 		{
-			tab[j] = (char *)malloc((word_len(s, i) + 1) * (sizeof(char)));
-			if (!tab[j])
-				return (NULL);
-			while (s[i] && s[i] != ' ' && s[i] != '\n' && s[i] != '\t')
-			{
-				tab[j][k] = s[i];
-				i++;
-				k++;
-			}
-			tab[j][k] = '\0';
-			j++;
-		}
+			split[j] = word_dup(s, flag, i);
+			if (!(split[j++]))
+				return (free_arr(split));
+			flag = -1;
+		}	
 	}
-	return (tab);
+	split[j] = NULL;
+	return (split);
 }
